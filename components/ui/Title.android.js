4a9eec3cd555e7cff27114087302769f8286d7e3
Ayeshmantha Madara Wijayagunethilake <ayesh99747@gmail.com>
2025-04-23 21:06:07 +0100

    Added different titles for android and ios

--- components/ui/Title.android.js
+++ components/ui/Title.android.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View, Platform} from "react-native";
 
 export default function Title({children}) {
     return (
@@ -14,7 +14,9 @@ const styles = StyleSheet.create({
         marginBottom: 24,
         textAlign: "center",
         color: 'white',
-        borderWidth: 2,
+        //borderWidth: Platform.OS === 'ios' ? 0 : 4,
+        //borderWidth: Platform.select({ios:0, android:4}),
+        borderWidth:4,
         borderColor: 'white',
         borderRadius: 4,
         padding: 12,
---
+++ components/ui/Title.ios.js
@@ -0,0 +1,28 @@
+import {StyleSheet, Text, View, Platform} from "react-native";
+
+export default function Title({children}) {
+    return (
+        <View>
+            <Text style={styles.title}>{children}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 24,
+        marginBottom: 24,
+        textAlign: "center",
+        color: 'white',
+        //borderWidth: Platform.OS === 'ios' ? 0 : 4,
+        //borderWidth: Platform.select({ios:0, android:4}),
+        borderWidth:0,
+
+        borderColor: 'white',
+        borderRadius: 4,
+        padding: 12,
+        fontFamily: 'open-sans-bold',
+        maxWidth: '80%',
+        width: 300,
+    },
+});
